f549317153844ac23f0dd650c22b098d1dd2149a
china-sty <1305120540@qq.com>
2026-04-13 21:05:45 +0800

    增加自动update逻辑

--- src/commands/upgrade.rs
+++ src/commands/upgrade.rs
@@ -869,8 +869,8 @@ pub fn maybe_schedule_background_update_check() {
 
 fn spawn_background_upgrade_process() -> bool {
     crate::utils::spawn_internal_git_ai_subcommand(
-        "upgrade",
-        &["--background"],
+        "update",
+        &[],
         ENV_BACKGROUND_UPGRADE_WORKER,
         &[],
     )
