b2e658193e481485a9eb29a7e21c1841c33cc895
shaoty1 <shaoty1@xiaopeng.com>
2026-05-09 20:23:02 +0800

    Revert "处理守护进程模式push未上传问题"
    This reverts commit fac7ec8e7febbde4381c3ddb366e949adf8a107f.

--- src/daemon.rs
+++ src/daemon.rs
@@ -1345,14 +1345,6 @@ fn apply_push_side_effect(
     let repo = find_repository_in_path(worktree)?;
     let parsed = parsed_invocation_for_side_effect(command, args);
     push_hooks::run_pre_push_hook_managed(&parsed, &repo);
-    
-    crate::utils::spawn_internal_git_ai_subcommand(
-        "upload-head-metrics",
-        &[],
-        "GIT_AI_BACKGROUND_METRICS_WORKER",
-        &[],
-    );
-
     Ok(())
 }
 
